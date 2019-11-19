05f960dade1d7abb31b0567a716643d07ff75dff
oyhoyhk <oyhoyhk@naver.com>
2019-11-19 21:55:31 +0900

    191119 ver3

--- js/main_re.js
+++ js/main_re.js
@@ -121,6 +121,45 @@ function init() {
 
         })
     })
+    // header 가운데 검색 기능 
+
+    let inputValue;
+    const input = document.getElementById(`main_search`);
+    input.addEventListener(`change`, function (e) {
+        inputValue = e.target.value;
+    });
+    const nextPageHref = document.querySelector(`.main_search a`).attributes[0].value;
+
+    input.addEventListener(`keydown`, function (e) {
+        if (e.key === `Enter`) {
+            if (inputValue === `자연`) {
+                window.location.href = nextPageHref + '0';
+            } else if (inputValue === `건축`) {
+                window.location.href = nextPageHref + '1';
+            } else if (inputValue === `인물`) {
+                window.location.href = nextPageHref + '2';
+            } else if (inputValue === `음식`) {
+                window.location.href = nextPageHref + '3';
+            } else {
+                alert(`자연, 건축, 인물, 음식 중에서 입력해주세요`);
+            }
+        }
+    });
+    const searchButton = document.querySelector(`.searchBox .search`);
+    searchButton.addEventListener(`click`, function () {
+        if (inputValue === `자연`) {
+            window.location.href = nextPageHref + '0';
+        } else if (inputValue === `건축`) {
+            window.location.href = nextPageHref + '1';
+        } else if (inputValue === `인물`) {
+            window.location.href = nextPageHref + '2';
+        } else if (inputValue === `음식`) {
+            window.location.href = nextPageHref + '3';
+        } else {
+            alert(`자연, 건축, 인물, 음식 중에서 입력해주세요`);
+        }
+    })
+
 
 
 }
--- js/sub1_re.js
+++ js/sub1_re.js
@@ -66,17 +66,18 @@ function init() {
         const photos = document.querySelector(`.photos`);
 
         for (let i = 0; i < length; i++) {
-            photos.innerHTML += `<div class="photo"><div class="cover"><div class="loc"></div></div></div>`;
+            photos.innerHTML += `<div class="photo"><div class="cover"><div class="loc"></div><div class="area"></div></div></div>`;
         }
         const photo = document.querySelectorAll(`.photo`);
         photo.forEach(function (elem, i) {
 
             const url = data.children[i].children[0].attributes.src.value;
             const category = data.children[i].children[1].children[2].innerHTML;
+            const area = data.children[i].children[1].children[1].innerHTML;
             const location = data.children[i].children[1].children[0].innerHTML;
             elem.style.backgroundImage = `url(${url})`;
             elem.children[0].children[0].innerHTML = location;
-
+            elem.children[0].children[1].innerHTML = area;
             const photoPopup = document.querySelector(`.photo_popup_background`);
             const photoLoc = document.querySelector(`#icon_location`);
             const photoBackground = document.querySelector(`.pic_area`);
@@ -103,7 +104,13 @@ function init() {
                     elem.style.display = `none`;
                 }
             }
+
         });
+        photo.forEach(function (elem) {
+            if (elem.style.display === 'none') {
+                elem.remove();
+            }
+        })
         document.querySelector(`.lists`).children[(categoryNumber === `#` ? -1 : categoryNumber) * 1 + 1].classList.add(`listOn`);
 
 
@@ -128,10 +135,76 @@ function init() {
             });
         });
 
+        const areas = document.querySelectorAll(`.area_list .area`);
+        console.log(areas)
+        let areaToggle = []; // false off , true on
+        for (let i = 0; i < 7; i++) {
+            areaToggle.push('');
+        }
+
+        areas.forEach(function (elem, i) {
+            elem.addEventListener(`click`, function () {
+                const photo = document.querySelectorAll(`.photo`);
+
+
+                if (areaToggle[i] === '') {
+                    elem.children[1].classList.add(`listOn`);
+                    areaToggle[i] = elem.children[0].innerHTML;
+                } else {
+                    elem.children[1].classList.remove(`listOn`)
+                    areaToggle[i] = '';
+                }
+                const areaText = elem.children[0].innerHTML;
+                photo.forEach(function (ph, i) {
+                    ph.style.display = `none`;
+                    const thisArea = ph.children[0].children[1].innerHTML;
+                    areaToggle.forEach(function (ar, i) {
+                        if (ar === thisArea) {
+                            ph.style.display = `inline-block`
+                        }
+                    })
+
+                })
+                if (i === 7) {
+                    areaToggle.forEach(function (elem1, i) {
+                        areaToggle[i] = '';
+                        areas[i].children[1].classList.remove(`listOn`);
+                    });
+                    photo.forEach(function (elem) {
+                        elem.style.display = `inline-block`;
+                    });
+                    console.log(areaToggle);
+                }
+
+
+                let checkedCount = 0;
+                for (let i = 0; i < areaToggle.length - 1; i++) {
+                    if (areaToggle[i] !== '') {
+                        checkedCount++;
+                    } else {}
+                }
+                if (checkedCount === 0) {
+                    photo.forEach(function (elem, i) {
+                        elem.style.display = `inline-block`;
+                    })
+                }
+                console.log(checkedCount);
+                if (checkedCount === 0) {
+                    photo.forEach(function (elem) {
+                        elem.style.display = `inline-block`;
+                    })
+                }
+
+            })
+        })
+
     }
 
     document.querySelector(`.photo_popup_exit_button`).addEventListener(`click`, function () {
         this.parentNode.style.display = `none`;
+    })
+    document.querySelector(`footer`).addEventListener(`click`, function () {
+        console.log(document.querySelectorAll(`.photo`).length)
     })
 
 }
